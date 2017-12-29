a6bbb541cab6f8de14ad54aa115eb20361e11df1
Sean <>
2017-12-29 17:48:13 +0800

    just modify article-list

--- vue-demo/vue-admin/src/views/article/article-list.vue
+++ vue-demo/vue-admin/src/views/article/article-list.vue
@@ -2,22 +2,28 @@
     <div class="article-list">
       <Card :bordered="false" dis-hover>
         <p slot="title">文章查询</p>
-        <Row>
+        <Row :gutter="10" type="flex" align="middle">
+          <Col span="2" style="text-align: right">文章标题：</Col>
           <Col span="4"><Input placeholder="请输入关键字" /></Col>
-          <Col span="4" offset="1">
+          <Col span="2" style="text-align: right">发布人：</Col>
+          <Col span="4">
             <Select placeholder="请选择发布人">
               <Option value="1">张三三</Option>
               <Option value="2">令狐冲</Option>
             </Select>
           </Col>
-          <Col span="4" offset="1"><Input placeholder="请输入关键字" /></Col>
-          <Col span="3" offset="1"><Button type="primary" style="margin-right: 30px">查询</Button></Col>
+          <Col span="2" style="text-align: right">发布时间：</Col>
+          <Col span="4"><DatePicker type="datetime" format="yyyy-MM-dd HH:mm" placeholder="请选择日期"></DatePicker></Col>
+          <Col span="2" style="text-align: right"><Button type="primary" style="margin-right: 30px">查询</Button></Col>
         </Row>
       </Card>
       <Card :bordered="false" dis-hover>
         <p slot="title">文章列表</p>
         <Table :columns="columnTitle" :data="articleList" border stripe></Table>
-        <Page :total="100" show-total style="padding: 10px 0; text-align: right;"></Page>
+        <Row class="table-footer">
+          <Col span="8"><Button type="error">删除</Button><Button type="primary">新增</Button></Col>
+          <Col span="16"><Page :total="100" show-total></Page></Col>
+        </Row>
       </Card>
     </div>
 </template>
@@ -45,6 +51,11 @@
             align: 'center'
           },
           {
+            title: '文章内容',
+            key: 'content',
+            align: 'center'
+          },
+          {
             title: '来源',
             key: 'source',
             align: 'center'
@@ -63,18 +74,9 @@
             title: '操作',
             key: 'action',
             align: 'center',
-            width: 180,
+            width: 140,
             render: (h, params) => {
               return h('div', [
-                h('Button', {
-                  props: { size: 'small' },
-                  style: { marginRight: '5px' },
-                  on: {
-                    click: () => {
-                      this.show(params.index)
-                    }
-                  }
-                }, '查看'),
                 h('Button', {
                   props: { size: 'small', type: 'primary' },
                   style: { marginRight: '5px' },
@@ -84,13 +86,22 @@
                   }
                 }, '编辑'),
                 h('Button', {
-                  props: { size: 'small', type: 'error' },
+                  props: { size: 'small' },
+                  style: { marginRight: '5px' },
                   on: {
                     click: () => {
-                      this.remove(params.index)
+                      this.show(params.index)
                     }
                   }
-                }, '删除')
+                }, '查看')
+//                h('Button', {
+//                  props: { size: 'small', type: 'error' },
+//                  on: {
+//                    click: () => {
+//                      this.remove(params.index)
+//                    }
+//                  }
+//                }, '删除')
               ])
             }
           }
@@ -100,7 +111,8 @@
             title: '这是一篇文章标题',
             source: '新华网',
             publisher: '张三三',
-            publishTime: '2017-12-27 09:33:34'
+            publishTime: '2017-12-27 09:33:34',
+            content: '这是什么鬼，如果文章内容超出了怎么办？这是什么鬼，如果文章内容超出了怎么办？'
           },
           {
             title: '这是一篇文章标题',
@@ -185,6 +197,15 @@
   .article-list {
     .ivu-card-head {
       /*background: #F0F0F0;*/
+    }
+    .table-footer {
+      margin-top: 16px;
+      .ivu-btn {
+        margin-right: 10px;
+      }
+      .ivu-page {
+        text-align: right;
+      }
     }
   }
 </style>
