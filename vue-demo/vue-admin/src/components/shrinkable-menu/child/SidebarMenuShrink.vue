210ce8a04ece8fdc60d58bf13a1c7081d21d7dad
Sean <>
2018-01-05 18:08:08 +0800

    modify SidebarMenuShrink

--- vue-demo/vue-admin/src/components/shrinkable-menu/child/SidebarMenuShrink.vue
+++ vue-demo/vue-admin/src/components/shrinkable-menu/child/SidebarMenuShrink.vue
@@ -1,14 +1,52 @@
 <template>
-    <div class="sidebar-menu-shrink"></div>
+  <div>
+    <template v-for="(item, index) in menuList">
+      <div style="text-align: center" :key="index">
+        <Dropdown v-if="item.children.length !== 1" placement="right-start" :key="index" @on-click="changeMenu">
+          <Button type="text">
+            <Icon :type="item.icon" :size="20"></Icon>
+          </Button>
+          <DropdownMenu slot="list">
+            <template v-for="(child, idx) in item.children">
+              <DropdownItem :name="child.name" :key="idx">
+                <Icon :type="child.icon"></Icon>
+                <span>{{ itemTitle(child) }}</span>
+              </DropdownItem>
+            </template>
+          </DropdownMenu>
+        </Dropdown>
+
+        <Dropdown transfer v-else placement="right-start" :name="item.name" :key="item.name">
+          <template slot="title">
+            <Icon :type="item.icon" :size="iconSize"></Icon>
+          </template>
+          <template v-for="(child, index) in item.children">
+            <MenuItem :name="child.name" :key="child.name">
+              <Icon :type="child.icon" :size="iconSize"></Icon>
+            </MenuItem>
+          </template>
+        </Dropdown>
+      </div>
+    </template>
+  </div>
 </template>
 <script>
   export default {
-    components: {},
-    data () {
-      return {}
+    props: {
+      menuList: Array,
+      iconSize: Number,
+      menuTheme: {
+        type: String,
+        default: 'dark'
+      },
+      openNames: {
+        type: Array
+      }
     },
-    created () {
-    },
-    methods: {}
+    methods: {
+      changeMenu (active) {
+        this.$emit('on-change', active)
+      }
+    }
   }
 </script>
